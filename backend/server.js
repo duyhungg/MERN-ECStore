afa03389ed585c04157f0feff4b6c8f734a59e63
Duy Hung <dongocduyhung7@gmail.com>
2023-05-25 16:16:59 +0700

    [25/05/2023]-Backend: settings env + connect mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const app = require("./app");
+const connectDatabase = require("./config/database");
 const dotenv = require("dotenv");
 // setting
 dotenv.config({ path: "backend/config/config.env" });
+// connect dataabase
+connectDatabase();
 app.listen(process.env.PORT, () => {
   console.log(
     `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
