3c24e0adc51d202794a5ae6f8f66923476232e05
Duy Hung <dongocduyhung7@gmail.com>
2023-05-26 15:10:39 +0700

    [26/05/2023]-Backend: CRUD - product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,91 @@
-exports.getProducts = (req, res, next) => {
+const Product = require("../models/product");
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+exports.getProducts = async (req, res, next) => {
+  const product = await Product.find();
   res.status(200).json({
     success: true,
-    message: "this route will show all products in  database",
+    count: product.length,
+    product,
   });
+};
+// Get single product details   =>   /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Sản phẩm không tồn tại.",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Đã xảy ra lỗi khi tìm kiếm sản phẩm.",
+      error: error.message,
+    });
+  }
+};
+exports.updateProduct = async (req, res, next) => {
+  try {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Sản phẩm không tồn tại.",
+      });
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Đã xảy ra lỗi khi updatesản phẩm.",
+      error: error.message,
+    });
+  }
+};
+// Delete Product   =>   /api/v1/admin/product/:id
+exports.deleteProduct = async (req, res, next) => {
+  try {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Sản phẩm không tồn tại.",
+      });
+    }
+    await product.deleteOne();
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Đã xảy ra lỗi khi updatesản phẩm.",
+      error: error.message,
+    });
+  }
 };
